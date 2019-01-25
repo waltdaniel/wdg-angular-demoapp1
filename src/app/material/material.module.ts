854b2441c691c3896f6d9b276d788da882c39505
WalterDG <wadagooo@gmail.com>
2019-01-25 13:27:25 -0300

    simple comp agregado correc dependencia

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [MaterialDemoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
