b4b0b3e8d7f6bd5b171c4dbf3b557f9d46e445ad
WalterDG <wadagooo@gmail.com>
2019-01-28 11:21:27 -0300

    agregada interfaz según modelo datos a utilizar según API de NASA.

---
+++ src/app/shared/model/apod.ts
@@ -0,0 +1,10 @@
+export interface Apod {
+    title: string;
+    date: string;
+    explanation: string;
+    url: string;
+    hdurl: string;
+    media_type: string;
+    service_version: string;
+
+}
